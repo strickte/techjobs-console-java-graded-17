3784a8d3433eda76fcd7800619b6eac36cc67ff3
strickte <166240135+strickte@users.noreply.github.com>
2024-10-16 11:03:33 -0500

    Passing 1 of 4 tests

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -124,12 +124,12 @@ public class TechJobs {
             System.out.println("No Results");
         } else {
             for (HashMap<String, String> job : someJobs) {
-                System.out.println("*****");
+                System.out.println("\n*****");
                 for (Map.Entry<String, String> jobListing : job.entrySet()) {
                     System.out.println(jobListing.getKey() +
                             ": " + jobListing.getValue());
                 }
-                System.out.println("*****\n");
+                System.out.println("*****");
             }
         }
     }
